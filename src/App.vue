44e298dead3065ff57c6b9413544b2c1f0366e3b
john.rey.n.decosta <john.rey.n.decosta@kccmalls.net>
2024-01-23 19:05:13 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <teleport to="#modal">
+    <div v-if="showModal">
+      <Modal header="Sign ups!" :data="arrayObject" theme="sale" @close="closeModalFunction">
+        <template v-slot:links>
+          <a href="#">signup now</a>
+          <a href="#">/moreinfo now</a>
+          <button @click="showModalFunction2">Show Modal</button>
+        </template>
+
+      </Modal>
+    </div>
+    <div v-if="showModalTwo">
+      <Modal header="Modal Two!" :data="arrayObject" theme="dark" @close="closeModalFunction2">
+      </Modal>
+    </div>
+  </teleport>
+
+    <button @click="showModalFunction">Show Modal</button>
+    <button @click="showModalFunction2">Show Modal 2</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Modal
+    // HelloWorld
+  },
+
+  data() {
+    return {
+      title: 'My first vue program',
+      arrayObject: {
+        name: "John Rey Decosta",
+        age: 16,
+        address: "Polomolok South Cotabato"
+      },
+      showModal: false,
+      showModalTwo: false
+
+    }
+  },
+  methods: {
+    showModalFunction() {
+      this.showModal = !this.showModal;
+    },
+    closeModalFunction(val) {
+      this.showModal = val;
+      alert(val);
+    },
+    showModalFunction2() {
+      this.showModalTwo = !this.showModalTwo;
+    },
+    closeModalFunction2(val) {
+      this.showModalTwo = val;
+
+    }
+
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="[theme === 'sale' ? 'sale' : 'dark']">
+            <h1>Modal Title {{ header }}</h1>
+            <h3>{{ data.name }}</h3>
+            <h3>{{ data.age }}</h3>
+            <h3>{{ data.address }}</h3>
+            <p>modal content</p>
+            <button>Close Modal</button>
+            <slot>Default</slot>
+            <div class="links">
+                <slot name="links">
+
+                </slot>
+            </div>
+
+            
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ModalView",
+    props: {
+        header: {
+            type: String
+        },
+        data: {
+            type: Object,
+
+        },
+        theme: {
+            type: String
+        }
+    },
+    methods: {
+        closeModal() {
+            this.$emit('close', false)
+        },
+
+    }
+}
+</script>
+
+<style scoped>
+.modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+}
+
+.backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.5);
+    width: 100%;
+    height: 100%;
+}
+
+h1 {
+    color: red;
+}
+
+.modal.sale {
+    background-color: crimson;
+    color: white;
+}
+
+.modal.sale h1 {
+    color: white
+}
+
+.modal.dark {
+    background-color: darkolivegreen;
+    color: white;
+}
+
+.modal.dark h1 {
+    color: white
+}
+</style>
